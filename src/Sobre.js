e0568b4fd3cd3dd6fd985fa7e7a4c82eb53f827f
ivansimplicio <ivansimplicio@outlook.com>
2020-12-17 18:10:33 -0300

    cadastro de projetos e ajustes nas páginas

--- src/Sobre.js
+++
@@ -1,9 +0,0 @@
-const Sobre = () => (
-    <div className="Sobre">
-        <h1>Sobre o Projeto</h1>
-        <hr />
-        <p>Informações do projetos, grupo, etc...</p>
-    </div>
-);
-
-export default Sobre;
--- src/views/Aluno.js
+++ src/views/Aluno.js
@@ -4,6 +4,7 @@ import logo from '../imagens/logo.png';
 import imgUpdate from '../imagens/update.png';
 import imgRead from '../imagens/read.png';
 import imgDelete from '../imagens/delete.png';
+import imgVoltar from '../imagens/voltar.png';
 
 const Aluno = () => {
     return(
@@ -31,6 +32,12 @@ const Aluno = () => {
 
               <div className="tile-icon"><img alt="delete" src={imgDelete}/></div>
               <div className="tile-title title-ltr">Excluir</div>
+          </a>
+
+          <a className="tile"  title="Voltar" href="/home">
+
+              <div className="tile-icon"><img alt="voltar" src={imgVoltar}/></div>
+              <div className="tile-title title-ltr">Voltar</div>
           </a>
       </div>
     </div>
